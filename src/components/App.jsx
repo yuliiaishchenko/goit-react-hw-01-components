c0696cd430f99e4dbad1ad5b5af70032686675e1
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-01 22:24:19 +0300

    friendList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import data from './Statistics/data.json';
 import { Statistics } from './Statistics/Statistics';
 
 import friends from './FriendList/friends.json';
-import { FriendList } from './FriendList/FriendList';
+import { FriendList } from './FriendList/FriendList/FriendList';
 
 import transactions from './TransactionHistory/transactions.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
