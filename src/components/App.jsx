ce03ccd477f1396bd30dde29f9ddbe2543cc19b8
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-01 22:09:19 +0300

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,12 @@ import { Profile } from './Profile/Profile';
 import data from './Statistics/data.json';
 import { Statistics } from './Statistics/Statistics';
 
+import friends from './FriendList/friends.json';
+import { FriendList } from './FriendList/FriendList/FriendList';
+
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
 
 
 export const App = () => {
@@ -27,7 +33,8 @@ export const App = () => {
   stats={user.stats}
 />
 <Statistics title="Upload stats" stats={data} />
-
+<FriendList friends={friends} />
+<TransactionHistory items={transactions} />
     </div>
   );
 };
