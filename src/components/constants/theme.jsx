aa75becfe0ed5666bc8b651bfffb9bcee946d48c
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-02 23:49:48 +0300

    theme

--- src/components/constants/theme.jsx
+++ src/components/constants/theme.jsx
@@ -4,6 +4,6 @@ export const theme = {
         black: '#000000',
         green: '#45b24a',
         red: '#ff4c55',
-
+        shadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
     }
 }
