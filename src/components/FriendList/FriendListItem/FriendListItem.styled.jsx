a9f5bf95038afae5b91cd7ef9ad64f8a687433f8
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-03 00:18:22 +0300

    fix

--- src/components/FriendList/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.styled.jsx
@@ -10,9 +10,9 @@ export const ItemsOfFriends = styled.li`
     width: 150px;
     padding: 10px;
     border-radius: 10px;
-    /* :hover {
-        box-shadow: ${({theme: { colors }}) => colors.shadow};
-    } */
+    :hover {
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+    }
 
 `;
 
