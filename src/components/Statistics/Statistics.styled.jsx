9c237ec74bc796059f3238b45e614ef856963251
yuliiaishchenko <romashka.ukr@gmail.com>
2023-05-03 01:54:38 +0300

    styled

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -44,6 +44,9 @@ export const StatListItem = styled.li`
     border-radius: 10px;
     padding: 5px;
     border: 2px solid azure;
+    :hover {
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+    }
 `
 
 export const Label = styled.span`
